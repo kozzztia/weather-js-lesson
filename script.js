51489efbff3312ae7e8adaa70ce636df2e815ff4
kozzztia <kozzztia@gmail.com>
2024-09-16 16:45:28 +0300

    change logic

--- script.js
+++ script.js
@@ -4,23 +4,10 @@ const geoApi = 'http://api.openweathermap.org/geo/1.0/direct';
 const weatherApi = 'http://api.openweathermap.org/data/2.5/weather';
 const preloader = document.getElementById('preloader');
 
-// preloader
-const loadingTexts = ["l","lo","loa","load","loadi","loadin", "loading", "loading.", "loading..","loading...",];
-let index = 0;
-
-function updateText() {
-    preloader.innerText = loadingTexts[index];
-    index = (index + 1) % loadingTexts.length;
-}
-
-setInterval(updateText, 100);
-
-// form handler
-
-cityForm.onsubmit = formHandler;
-
+// classes
 class WeatherData {
-    constructor(data) {
+    constructor(data, city) {
+        this.cityName = city;
         this.coord = data.coord;
         this.dateForRestart = this.getDateForRestart();
         this.country = data.sys.country;
@@ -37,11 +24,12 @@ class WeatherData {
     getDateForRestart() {
         const now = new Date();
         now.setHours(now.getHours() + 2);
-        return now.toLocaleTimeString();
+        return now.toLocaleTimeString()
     }
 
     setDataToLocalStorage() {
         const weatherData = {
+            cityName: this.cityName,
             coord: this.coord,
             dateForRestart: this.dateForRestart,
             country: this.country,
@@ -58,35 +46,70 @@ class WeatherData {
         window.localStorage.setItem('weather', JSON.stringify(weatherData));
     }
 
-    getDataFromLocalStorage() {
-        const storedData = window.localStorage.getItem('weather');
-        if (storedData) {
-            return JSON.parse(storedData);
+}
+
+class LoadingAnimation {
+    constructor(preloaderElement, loadingTexts, intervalTime = 300) {
+        this.preloader = preloaderElement;
+        this.loadingTexts = loadingTexts;
+        this.intervalTime = intervalTime;
+        this.index = 0;
+        this.loadingInterval = null;
+    }
+    start() {
+        this.preloader.style.display = "block"; 
+        this.index = 0;
+        this.loadingInterval = setInterval(() => this.updateText(), this.intervalTime); 
+    }
+    stop() {
+        if (this.loadingInterval) {
+            clearInterval(this.loadingInterval); 
+            this.loadingInterval = null; 
         }
-        return null;
+        this.preloader.style.display = "none"; 
+    }
+
+    updateText() {
+        this.preloader.innerText = this.loadingTexts[this.index];
+        this.index = (this.index + 1) % this.loadingTexts.length;
     }
 }
 
+// preloader
+const loadingTexts = ["l","lo","loa","load","loadi","loadin", "loading", "loading.", "loading..","loading...",];
+const loadingAnimation = new LoadingAnimation(preloader, loadingTexts, 300);
+
+
+// form handler
+
+cityForm.onsubmit = formHandler;
+
 async function formHandler(e) {
     e.preventDefault();
-    let city = e.target.city;
+    let city = e.target.city.value.trim();
 
-    if (city.value.length > 3) {
-        const weatherData = await getCityWeather(city.value);
-        if (weatherData) {
-            const cityWeather = new WeatherData(weatherData);
+    if (city.length > 3) {
+        loadingAnimation.start();
 
-            cityWeather.setDataToLocalStorage();
+        try {
+            const data = await getCityWeather(city);
+            if (data) {
+                const weatherData = new WeatherData(data, city); 
+                weatherData.setDataToLocalStorage();
 
-            const cityWeatherFromLocalStorage = cityWeather.getDataFromLocalStorage();
-
-            console.log(cityWeatherFromLocalStorage); 
+                const weather = localStorage.getItem('weather');
+                console.log(JSON.parse(weather));
+            }
+        } catch (error) {
+            console.error("error when try to get information:", error);
+        } finally {
+            loadingAnimation.stop(); 
         }
     } else {
-        city.placeholder = "Insert correct city";
+        e.target.city.placeholder = "insert correct city";
     }
 
-    city.value = "";
+    e.target.city.value = "";
 }
 
 async function getCityWeather(city) {
