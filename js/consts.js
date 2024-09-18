dc22ee7f2b3652eacbb4dbee383a55b15ba095a6
kozzztia <kozzztia@gmail.com>
2024-09-18 11:15:16 +0300

    fix https and button

--- js/consts.js
+++ js/consts.js
@@ -1,4 +1,4 @@
 export const loadingTexts = ["l", "lo", "loa", "load", "loadi", "loadin", "loading", "loading.", "loading..", "loading..."];
 export const key = 'b10a1709279e52171ae6535fa8b02312';
-export const geoApi = 'http://api.openweathermap.org/geo/1.0/direct';
-export const weatherApi = 'http://api.openweathermap.org/data/2.5/weather';
+export const geoApi = 'https://api.openweathermap.org/geo/1.0/direct';
+export const weatherApi = 'https://api.openweathermap.org/data/2.5/weather';
